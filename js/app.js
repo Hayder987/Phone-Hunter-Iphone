9d15fcf2e773870d43fe0695b2f9d1b12684b02b
hayder <hayderbd4290@gmail.com>
2024-10-10 19:09:10 +0600

    card Details Added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 
 const cardDiv = document.getElementById("card");
+
 const getPhones = async(brand)=>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${brand}`;
     const res = await fetch(url);
@@ -11,6 +12,38 @@ const getPhones = async(brand)=>{
     });
 }
 
+const getDetails = async(slug)=>{
+    const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    detailsDisplay(data.data);
+};
+
+const detailsDisplay=(item)=>{
+   document.getElementById("modal1").show();
+    const modalDetails = document.getElementById("modalDetails");
+    modalDetails.innerHTML = `
+       <div class= "flex mb-4 justify-center items-center bg-slate-200 p-3 rounded-xl">
+         <img class="rounded-2xl" src=${item.image}>
+       </div>
+       <h1 class="text-center text-2xl font-bold mb-4">${item.name}</h1>
+       <p class="text-xs"><span class="text-base font-bold">Storage:</span> ${item?.mainFeatures?.storage || "Data Not Found"}</p>
+       <p class="text-xs"><span class="text-base font-bold">Display:</span> ${item?.mainFeatures?.displaySize || "Data Not Found"}</p>
+       <p class="text-xs"><span class="text-base font-bold">ChipSet:</span> ${item?.mainFeatures?.chipSet || "Data Not Found"}</p>
+       <p class="text-xs"><span class="text-base font-bold">Memory:</span> ${item.mainFeatures?.memory || "Data Not Found"}</p>
+       <p class="text-xs"><span class="text-base font-bold">Slug:</span> ${item?.slug || "Data Not Found"}</p>
+       <p class="text-xs"><span class="text-base font-bold">Release-Date:</span> ${item?.releaseDate || "Data Not Found"}</p>
+       <p class="text-xs"><span class="text-base font-bold">Brand:</span> ${item?.brand || "Data Not Found"}</p>
+       <p class="text-xs"><span class="text-base font-bold">GPS:</span> ${item?.others?.GPS || "Data Not Found"}</p>
+       <p class="text-xs"><span class="text-base font-bold">WLAN:</span> ${item?.others?.WLAN || "Data Not Found"}</p>
+       <p class="text-xs"><span class="text-base font-bold">Bluetooth:</span> ${item?.others?.Bluetooth || "Data Not Found"}</p>
+       <p class="text-xs"><span class="text-base font-bold">USB:</span> ${item?.others?.USB || "Data Not Found"}</p>
+
+
+    `
+    modalDetails.appendChild(div);
+
+}
 
 
 const displayPhones =(condition, phones)=>{
@@ -36,7 +69,7 @@ const displayPhones =(condition, phones)=>{
        <h3 class="text-xl text-center text-gray-400 font-semibold mb-4">Slug: ${item.slug}</h3>
        <h2 class="text-center text-2xl mb-4 font-bold text-blue-500">Brand: ${item.brand}</h2>
        <div class=" flex justify-center items-center mb-4">
-         <button id="deatilsBtn" class="bg-btnBg py-2 px-6 rounded-xl ml-[-20px] text-white font-bold">Show Details</button>
+         <button onclick="getDetails('${item.slug}')" class="bg-btnBg py-2 px-6 rounded-xl ml-[-20px] text-white font-bold">Show Details</button>
        </div>
      </div>
 
